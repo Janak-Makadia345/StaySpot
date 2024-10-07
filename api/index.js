9b4b1ba00abd25700827094fe0978afe68c72f82
Janak Makadia <147591737+Janak-Makadia345@users.noreply.github.com>
2024-10-07 21:42:43 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,14 +12,10 @@ const app = express();
 dotenv.config();
 
 const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB.");
-  } catch (error) {
-    throw error;
-  }
-};
-
+ 
+await mongoose.connect(process.env.MONGO);
+console.log("Connected to mongoDB.");
+  
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
 });
